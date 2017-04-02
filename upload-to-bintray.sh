ed5d309978ca1c179d3d4ba4616d298478788aa1
Drilon Reçica <drilonreqica@gmail.com>
2017-04-02 17:35:10 +0200

    Testing bintray deployment (Test2)

--- upload-to-bintray.sh
+++ upload-to-bintray.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 echo -e "Start clean module"
-./gradlew clean
+./gradlew :securestorage:clean
 echo -e "Finished clean"
 
 echo -e "Start install module"
-./gradlew install
+./gradlew :securestorage:install
 echo -e "Finished install"
+
+echo -e "Start bintrayUpload"
+./gradlew :securestorage:bintrayUpload
+echo -e "Finished bintrayUpload"
 
