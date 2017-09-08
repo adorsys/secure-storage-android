2006f15b199af4d3a09956a3fc8dc951fcbc8cb3
Nino Handler <nih@adorsys.de>
2017-09-08 16:23:54 +0200

    Optimized MainActivity

--- app/src/main/java/de/adorsys/android/securestoragetest/MainActivity.kt
+++ app/src/main/java/de/adorsys/android/securestoragetest/MainActivity.kt
@@ -19,22 +19,23 @@ class MainActivity : AppCompatActivity() {
         private val TAG = "LOGTAG"
     }
 
+    private lateinit var inputEditText: EditText
+    private lateinit var keyInfoTextView: TextView
+    private lateinit var generateKeyButton: Button
+    private lateinit var clearPreferencesButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val inputEditText = findViewById(R.id.plain_message_edit_text) as EditText
-        val keyInfoTextView = findViewById(R.id.key_info_text_view) as TextView
-        val generateKeyButton = findViewById(R.id.generate_key_button) as Button
-        val clearPreferencesButton = findViewById(R.id.clear_preferences_button) as Button
+        inputEditText = findViewById(R.id.plain_message_edit_text) as EditText
+        keyInfoTextView = findViewById(R.id.key_info_text_view) as TextView
+        generateKeyButton = findViewById(R.id.generate_key_button) as Button
+        clearPreferencesButton = findViewById(R.id.clear_preferences_button) as Button
 
         generateKeyButton.setOnClickListener {
             if (!TextUtils.isEmpty(inputEditText.text)) {
-                EncryptTask(
-                        WeakReference(this),
-                        WeakReference(generateKeyButton),
-                        WeakReference(inputEditText),
-                        WeakReference(keyInfoTextView)).execute()
+                EncryptAsyncTask(WeakReference(this)).execute()
             } else {
                 Toast.makeText(this@MainActivity, "Field cannot be empty", Toast.LENGTH_SHORT).show()
             }
@@ -62,25 +63,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    class EncryptTask(private val activity: WeakReference<MainActivity>,
-                      private val generateKeyButton: WeakReference<Button>,
-                      private val inputEditText: WeakReference<EditText>,
-                      private val keyInfoTextView: WeakReference<TextView>) : AsyncTask<Void, Boolean, Boolean>() {
+    class EncryptAsyncTask(private val activity: WeakReference<MainActivity>) : AsyncTask<Void, Boolean, Boolean>() {
         private val handler = Handler(Looper.getMainLooper())
         private var decryptedMessage: String? = null
 
         override fun doInBackground(vararg params: Void?): Boolean? {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
                 val activity = activity.get() ?: return false
-                val generateKeyButton = generateKeyButton.get() ?: return false
-                val inputEditText = inputEditText.get() ?: return false
+                val generateKeyButton = activity.generateKeyButton
+                val inputEditText = activity.inputEditText
                 if (generateKeyButton.text.toString() == activity.getString(R.string.button_generate_encrypt)) {
                     generateKeyButton.setText(R.string.button_encrypt)
                 }
                 try {
-
                     SecurePreferences.setValue(activity, KEY, inputEditText.text.toString())
-
                     decryptedMessage = SecurePreferences.getStringValue(activity, KEY, "")
                     if (BuildConfig.DEBUG) {
                         Log.d(TAG, decryptedMessage!! + " ")
@@ -98,8 +94,8 @@ class MainActivity : AppCompatActivity() {
 
         override fun onPostExecute(result: Boolean?) {
             val activity = activity.get() ?: return
-            val keyInfoTextView = keyInfoTextView.get() ?: return
-            val inputEditText = inputEditText.get() ?: return
+            val keyInfoTextView = activity.keyInfoTextView
+            val inputEditText = activity.inputEditText
             keyInfoTextView.text = activity.getString(R.string.message_encrypted_decrypted,
                     inputEditText.text.toString(), decryptedMessage)
         }
