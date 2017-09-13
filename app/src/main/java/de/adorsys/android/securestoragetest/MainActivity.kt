4e945b7208bd030c85e836526279c69f60766160
Drilon Reçica <drilonreqica@gmail.com>
2017-09-13 16:01:18 +0200

    Minor formatting changes

--- app/src/main/java/de/adorsys/android/securestoragetest/MainActivity.kt
+++ app/src/main/java/de/adorsys/android/securestoragetest/MainActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 adorsys GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.adorsys.android.securestoragetest
 
 import android.os.*
@@ -43,7 +58,7 @@ class MainActivity : AppCompatActivity() {
 
         clearPreferencesButton.setOnClickListener {
             try {
-                SecurePreferences.clearAllValues(this@MainActivity)
+                SecurePreferences.clearAllValues()
                 Toast.makeText(this@MainActivity, "SecurePreferences cleared and KeyPair deleted", Toast.LENGTH_SHORT).show()
                 keyInfoTextView.text = ""
             } catch (e: Exception) {
@@ -76,8 +91,8 @@ class MainActivity : AppCompatActivity() {
                     generateKeyButton.setText(R.string.button_encrypt)
                 }
                 try {
-                    SecurePreferences.setValue(activity, KEY, inputEditText.text.toString())
-                    decryptedMessage = SecurePreferences.getStringValue(activity, KEY, "")
+                    SecurePreferences.setValue(KEY, inputEditText.text.toString())
+                    decryptedMessage = SecurePreferences.getStringValue(KEY, "")
                     if (BuildConfig.DEBUG) {
                         Log.d(TAG, decryptedMessage!! + " ")
                     }
