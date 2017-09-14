ac72716b554074e6d006994537fd0ba1403de9f5
Drilon Reçica <drilonreqica@gmail.com>
2017-09-14 11:40:12 +0200

    Fixed missing resource crash

--- securestoragelibrary/src/main/java/de/adorsys/android/securestoragelibrary/KeystoreTool.java
+++ securestoragelibrary/src/main/java/de/adorsys/android/securestoragelibrary/KeystoreTool.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package de.adorsys.android.securestoragelibrary;
 
 import android.content.res.Configuration;
@@ -43,208 +44,193 @@ import javax.crypto.CipherOutputStream;
 import javax.security.auth.x500.X500Principal;
 
 import static android.os.Build.VERSION_CODES.M;
-import static de.adorsys.android.securestoragelibrary.SecureStorageProvider.context;
 import static de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.CRYPTO_EXCEPTION;
 import static de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.INTERNAL_LIBRARY_EXCEPTION;
 import static de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.KEYSTORE_EXCEPTION;
-import static de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.KEYSTORE_NOT_SUPPORTED_EXCEPTION;
+import static de.adorsys.android.securestoragelibrary.SecureStorageProvider.context;
 
 final class KeystoreTool {
-	private static final String KEY_ALIAS = "adorsysKeyPair";
-	private static final String KEY_ENCRYPTION_ALGORITHM = "RSA";
-	private static final String KEY_CHARSET = "UTF-8";
-	private static final String KEY_KEYSTORE_NAME = "AndroidKeyStore";
-	private static final String KEY_CIPHER_JELLYBEAN_PROVIDER = "AndroidOpenSSL";
-	private static final String KEY_CIPHER_MARSHMALLOW_PROVIDER = "AndroidKeyStoreBCWorkaround";
-	private static final String KEY_TRANSFORMATION_ALGORITHM = "RSA/ECB/PKCS1Padding";
-	private static final String KEY_X500PRINCIPAL = "CN=SecureDeviceStorage, O=Adorsys, C=Germany";
+    private static final String KEY_ALIAS = "adorsysKeyPair";
+    private static final String KEY_ENCRYPTION_ALGORITHM = "RSA";
+    private static final String KEY_CHARSET = "UTF-8";
+    private static final String KEY_KEYSTORE_NAME = "AndroidKeyStore";
+    private static final String KEY_CIPHER_JELLYBEAN_PROVIDER = "AndroidOpenSSL";
+    private static final String KEY_CIPHER_MARSHMALLOW_PROVIDER = "AndroidKeyStoreBCWorkaround";
+    private static final String KEY_TRANSFORMATION_ALGORITHM = "RSA/ECB/PKCS1Padding";
+    private static final String KEY_X500PRINCIPAL = "CN=SecureDeviceStorage, O=Adorsys, C=Germany";
 
-	// hidden constructor to disable initialization
-	private KeystoreTool() {
-	}
+    // hidden constructor to disable initialization
+    private KeystoreTool() {
+    }
 
-	@Nullable
-	static String encryptMessage(@NonNull String plainMessage) throws SecureStorageException {
-		try {
-			Cipher input;
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2
-					&& Build.VERSION.SDK_INT < M) {
-				input = Cipher.getInstance(KEY_TRANSFORMATION_ALGORITHM, KEY_CIPHER_JELLYBEAN_PROVIDER);
-			} else if (Build.VERSION.SDK_INT >= M) {
-				input = Cipher.getInstance(KEY_TRANSFORMATION_ALGORITHM, KEY_CIPHER_MARSHMALLOW_PROVIDER);
-			} else {
-				Log.e(KeystoreTool.class.getName(), context.get().getString(R.string.message_supported_api));
-				throw new SecureStorageException(context.get().getString(R.string.message_supported_api), null, KEYSTORE_NOT_SUPPORTED_EXCEPTION);
-			}
-			input.init(Cipher.ENCRYPT_MODE, getPublicKey());
+    @Nullable
+    static String encryptMessage(@NonNull String plainMessage) throws SecureStorageException {
+        try {
+            Cipher input;
+            if (Build.VERSION.SDK_INT >= M) {
+                input = Cipher.getInstance(KEY_TRANSFORMATION_ALGORITHM, KEY_CIPHER_MARSHMALLOW_PROVIDER);
+            } else {
+                input = Cipher.getInstance(KEY_TRANSFORMATION_ALGORITHM, KEY_CIPHER_JELLYBEAN_PROVIDER);
+            }
+            input.init(Cipher.ENCRYPT_MODE, getPublicKey());
 
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			CipherOutputStream cipherOutputStream = new CipherOutputStream(
-					outputStream, input);
-			cipherOutputStream.write(plainMessage.getBytes(KEY_CHARSET));
-			cipherOutputStream.close();
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            CipherOutputStream cipherOutputStream = new CipherOutputStream(
+                    outputStream, input);
+            cipherOutputStream.write(plainMessage.getBytes(KEY_CHARSET));
+            cipherOutputStream.close();
 
-			byte[] values = outputStream.toByteArray();
-			return Base64.encodeToString(values, Base64.DEFAULT);
+            byte[] values = outputStream.toByteArray();
+            return Base64.encodeToString(values, Base64.DEFAULT);
 
-		} catch (Exception e) {
-			throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
-		}
-	}
+        } catch (Exception e) {
+            throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
+        }
+    }
 
-	@NonNull
-	static String decryptMessage(@NonNull String encryptedMessage) throws SecureStorageException {
-		try {
-			Cipher output;
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2
-					&& Build.VERSION.SDK_INT < M) {
-				output = Cipher.getInstance(KEY_TRANSFORMATION_ALGORITHM, KEY_CIPHER_JELLYBEAN_PROVIDER);
-			} else if (Build.VERSION.SDK_INT >= M) {
-				output = Cipher.getInstance(KEY_TRANSFORMATION_ALGORITHM, KEY_CIPHER_MARSHMALLOW_PROVIDER);
-			} else {
-				Log.e(KeystoreTool.class.getName(), context.get().getString(R.string.message_supported_api));
-				throw new SecureStorageException(context.get().getString(R.string.message_supported_api), null, KEYSTORE_NOT_SUPPORTED_EXCEPTION);
-			}
+    @NonNull
+    static String decryptMessage(@NonNull String encryptedMessage) throws SecureStorageException {
+        try {
+            Cipher output;
+            if (Build.VERSION.SDK_INT >= M) {
+                output = Cipher.getInstance(KEY_TRANSFORMATION_ALGORITHM, KEY_CIPHER_MARSHMALLOW_PROVIDER);
+            } else {
+                output = Cipher.getInstance(KEY_TRANSFORMATION_ALGORITHM, KEY_CIPHER_JELLYBEAN_PROVIDER);
+            }
+            output.init(Cipher.DECRYPT_MODE, getPrivateKey());
 
-			output.init(Cipher.DECRYPT_MODE, getPrivateKey());
+            CipherInputStream cipherInputStream = new CipherInputStream(
+                    new ByteArrayInputStream(Base64.decode(encryptedMessage, Base64.DEFAULT)), output);
+            List<Byte> values = new ArrayList<>();
 
-			CipherInputStream cipherInputStream = new CipherInputStream(
-					new ByteArrayInputStream(Base64.decode(encryptedMessage, Base64.DEFAULT)), output);
-			List<Byte> values = new ArrayList<>();
+            int nextByte;
+            while ((nextByte = cipherInputStream.read()) != -1) { //NOPMD
+                values.add((byte) nextByte);
+            }
 
-			int nextByte;
-			while ((nextByte = cipherInputStream.read()) != -1) {
-				values.add((byte) nextByte);
-			}
+            byte[] bytes = new byte[values.size()];
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = values.get(i);
+            }
 
-			byte[] bytes = new byte[values.size()];
-			for (int i = 0; i < bytes.length; i++) {
-				bytes[i] = values.get(i);
-			}
+            return new String(bytes, 0, bytes.length, KEY_CHARSET);
 
-			return new String(bytes, 0, bytes.length, KEY_CHARSET);
+        } catch (Exception e) {
+            throw new SecureStorageException(e.getMessage(), e, CRYPTO_EXCEPTION);
+        }
+    }
 
-		} catch (Exception e) {
-			throw new SecureStorageException(e.getMessage(), e, CRYPTO_EXCEPTION);
-		}
-	}
+    static boolean keyPairExists() throws SecureStorageException {
+        try {
+            return getKeyStoreInstance().getKey(KEY_ALIAS, null) != null;
+        } catch (Exception e) {
+            throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
+        }
+    }
 
-	static boolean keyPairExists() throws SecureStorageException {
-		try {
-			return getKeyStoreInstance().getKey(KEY_ALIAS, null) != null;
-		} catch (Exception e) {
-			throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
-		}
-	}
+    static void generateKeyPair() throws SecureStorageException {
+        // Create new key if needed
+        if (!keyPairExists()) {
+            generateAsymmetricKeyPair();
+        } else if (BuildConfig.DEBUG) {
+            Log.e(KeystoreTool.class.getName(),
+                    context.get().getString(R.string.message_keypair_already_exists));
+        }
+    }
 
-	static void generateKeyPair() throws SecureStorageException {
-		// Create new key if needed
-		if (!keyPairExists()) {
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-				generateAsymmetricKeyPair();
-			} else {
-				Log.e(KeystoreTool.class.getName(), context.get().getString(R.string.message_supported_api));
-				throw new SecureStorageException(context.get().getString(R.string.message_supported_api), null, KEYSTORE_NOT_SUPPORTED_EXCEPTION);
-			}
-		} else if (BuildConfig.DEBUG) {
-			Log.e(KeystoreTool.class.getName(),
-					context.get().getString(R.string.message_keypair_already_exists));
-		}
-	}
+    static void deleteKeyPair() throws SecureStorageException {
+        // Delete Key from Keystore
+        if (keyPairExists()) {
+            try {
+                getKeyStoreInstance().deleteEntry(KEY_ALIAS);
+            } catch (KeyStoreException e) {
+                throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
+            }
+        } else if (BuildConfig.DEBUG) {
+            Log.e(KeystoreTool.class.getName(),
+                    context.get().getString(R.string.message_keypair_does_not_exist));
+        }
+    }
 
-	static void deleteKeyPair() throws SecureStorageException {
-		// Delete Key from Keystore
-		if (keyPairExists()) {
-			try {
-				getKeyStoreInstance().deleteEntry(KEY_ALIAS);
-			} catch (KeyStoreException e) {
-				throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
-			}
-		} else if (BuildConfig.DEBUG) {
-			Log.e(KeystoreTool.class.getName(),
-					context.get().getString(R.string.message_keypair_does_not_exist));
-		}
-	}
+    @Nullable
+    private static PublicKey getPublicKey() throws SecureStorageException {
+        try {
+            if (keyPairExists()) {
+                KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) getKeyStoreInstance().getEntry(KEY_ALIAS, null);
+                return privateKeyEntry.getCertificate().getPublicKey();
+            } else {
+                if (BuildConfig.DEBUG) {
+                    Log.e(KeystoreTool.class.getName(), context.get().getString(R.string.message_keypair_does_not_exist));
+                }
+                throw new SecureStorageException(context.get().getString(R.string.message_keypair_does_not_exist), null, INTERNAL_LIBRARY_EXCEPTION);
+            }
+        } catch (Exception e) {
+            throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
+        }
+    }
 
-	@Nullable
-	private static PublicKey getPublicKey() throws SecureStorageException {
-		try {
-			if (keyPairExists()) {
-				KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) getKeyStoreInstance().getEntry(KEY_ALIAS, null);
-				return privateKeyEntry.getCertificate().getPublicKey();
-			} else {
-				if (BuildConfig.DEBUG) {
-					Log.e(KeystoreTool.class.getName(), context.get().getString(R.string.message_keypair_does_not_exist));
-				}
-				throw new SecureStorageException(context.get().getString(R.string.message_keypair_does_not_exist), null, INTERNAL_LIBRARY_EXCEPTION);
-			}
-		} catch (Exception e) {
-			throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
-		}
-	}
+    @Nullable
+    private static PrivateKey getPrivateKey() throws SecureStorageException {
+        try {
+            if (keyPairExists()) {
+                KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) getKeyStoreInstance().getEntry(KEY_ALIAS, null);
+                return privateKeyEntry.getPrivateKey();
+            } else {
+                if (BuildConfig.DEBUG) {
+                    Log.e(KeystoreTool.class.getName(), context.get().getString(R.string.message_keypair_does_not_exist));
+                }
+                throw new SecureStorageException(context.get().getString(R.string.message_keypair_does_not_exist), null, INTERNAL_LIBRARY_EXCEPTION);
+            }
+        } catch (Exception e) {
+            throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
+        }
+    }
 
-	@Nullable
-	private static PrivateKey getPrivateKey() throws SecureStorageException {
-		try {
-			if (keyPairExists()) {
-				KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) getKeyStoreInstance().getEntry(KEY_ALIAS, null);
-				return privateKeyEntry.getPrivateKey();
-			} else {
-				if (BuildConfig.DEBUG) {
-					Log.e(KeystoreTool.class.getName(), context.get().getString(R.string.message_keypair_does_not_exist));
-				}
-				throw new SecureStorageException(context.get().getString(R.string.message_keypair_does_not_exist), null, INTERNAL_LIBRARY_EXCEPTION);
-			}
-		} catch (Exception e) {
-			throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
-		}
-	}
+    private static boolean isRTL() {
+        Configuration config = context.get().getResources().getConfiguration();
+        return config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL;
+    }
 
-	private static boolean isRTL() {
-		Configuration config = context.get().getResources().getConfiguration();
-		return config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL;
-	}
+    private static void generateAsymmetricKeyPair() throws SecureStorageException {
+        try {
+            if (isRTL()) {
+                Locale.setDefault(Locale.US);
+            }
 
-	private static void generateAsymmetricKeyPair() throws SecureStorageException {
-		try {
-			if (isRTL()) {
-				Locale.setDefault(Locale.US);
-			}
+            Calendar start = Calendar.getInstance();
+            Calendar end = Calendar.getInstance();
+            end.add(Calendar.YEAR, 99);
 
-			Calendar start = Calendar.getInstance();
-			Calendar end = Calendar.getInstance();
-			end.add(Calendar.YEAR, 99);
+            KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(context.get())
+                    .setAlias(KEY_ALIAS)
+                    .setSubject(new X500Principal(KEY_X500PRINCIPAL))
+                    .setSerialNumber(BigInteger.TEN)
+                    .setStartDate(start.getTime())
+                    .setEndDate(end.getTime())
+                    .build();
 
-			KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(context.get())
-					.setAlias(KEY_ALIAS)
-					.setSubject(new X500Principal(KEY_X500PRINCIPAL))
-					.setSerialNumber(BigInteger.TEN)
-					.setStartDate(start.getTime())
-					.setEndDate(end.getTime())
-					.build();
+            KeyPairGenerator generator
+                    = KeyPairGenerator.getInstance(KEY_ENCRYPTION_ALGORITHM, KEY_KEYSTORE_NAME);
+            generator.initialize(spec);
+            generator.generateKeyPair();
+        } catch (Exception e) {
+            throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
+        }
+    }
 
-			KeyPairGenerator generator
-					= KeyPairGenerator.getInstance(KEY_ENCRYPTION_ALGORITHM, KEY_KEYSTORE_NAME);
-			generator.initialize(spec);
-			generator.generateKeyPair();
-		} catch (Exception e) {
-			throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
-		}
-	}
+    @NonNull
+    private static KeyStore getKeyStoreInstance() throws SecureStorageException {
+        try {
+            // Get the AndroidKeyStore instance
+            KeyStore keyStore = KeyStore.getInstance(KEY_KEYSTORE_NAME);
 
-	@NonNull
-	private static KeyStore getKeyStoreInstance() throws SecureStorageException {
-		try {
-			// Get the AndroidKeyStore instance
-			KeyStore keyStore = KeyStore.getInstance(KEY_KEYSTORE_NAME);
+            // Relict of the JCA API - you have to call load even
+            // if you do not have an input stream you want to load or it'll crash
+            keyStore.load(null);
 
-			// Relict of the JCA API - you have to call load even
-			// if you do not have an input stream you want to load or it'll crash
-			keyStore.load(null);
-
-			return keyStore;
-		} catch (Exception e) {
-			throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
-		}
-	}
+            return keyStore;
+        } catch (Exception e) {
+            throw new SecureStorageException(e.getMessage(), e, KEYSTORE_EXCEPTION);
+        }
+    }
 }
