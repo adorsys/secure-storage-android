4e2951c3c45e889aa6d4ba7c64796cc582e6b85b
Drilon Reçica <drilonreqica@gmail.com>
2018-09-21 14:15:42 +0200

    Fixed pmd and checkstyle issues

--- app/src/main/java/de/adorsys/android/securestoragetest/MainActivity.kt
+++ app/src/main/java/de/adorsys/android/securestoragetest/MainActivity.kt
@@ -131,6 +131,7 @@ class MainActivity : AppCompatActivity() {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             Html.fromHtml(text, Html.FROM_HTML_MODE_COMPACT)
         } else {
+            @Suppress("DEPRECATION")
             Html.fromHtml(text)
         }
     }
