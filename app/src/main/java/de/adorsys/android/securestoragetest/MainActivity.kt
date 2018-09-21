5f49d64824320400b3518cb34e8e8727d3de41d6
Drilon Reçica <drilonreqica@gmail.com>
2018-09-21 14:08:56 +0200

    Fixed Android P warning when getting Public or Private key from KeystoreInstance

--- app/src/main/java/de/adorsys/android/securestoragetest/MainActivity.kt
+++ app/src/main/java/de/adorsys/android/securestoragetest/MainActivity.kt
@@ -32,7 +32,10 @@ import android.view.animation.Animation
 import android.widget.Toast
 import de.adorsys.android.securestoragelibrary.SecurePreferences
 import de.adorsys.android.securestoragelibrary.SecureStorageException
-import de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.*
+import de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.CRYPTO_EXCEPTION
+import de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.INTERNAL_LIBRARY_EXCEPTION
+import de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.KEYSTORE_EXCEPTION
+import de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.KEYSTORE_NOT_SUPPORTED_EXCEPTION
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
