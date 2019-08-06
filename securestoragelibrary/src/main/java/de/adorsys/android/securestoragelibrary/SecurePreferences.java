8dca1ada437c71cb7c154e62a889a6239e095cdc
drilonrecica <drilonrecica.dev@gmail.com>
2019-08-06 15:25:22 +0200

    Wrote Documentation for visible classes

--- securestoragelibrary/src/main/java/de/adorsys/android/securestoragelibrary/SecurePreferences.java
+++ securestoragelibrary/src/main/java/de/adorsys/android/securestoragelibrary/SecurePreferences.java
@@ -29,6 +29,10 @@ import androidx.annotation.Nullable;
 import static android.content.Context.MODE_PRIVATE;
 import static de.adorsys.android.securestoragelibrary.SecureStorageException.ExceptionType.CRYPTO_EXCEPTION;
 
+/**
+ * Handles every use case for the developer using Secure Storage.
+ * Encryption, Decryption, Storage, Removal etc.
+ */
 public final class SecurePreferences {
     private static final String KEY_SHARED_PREFERENCES_NAME = "SecurePreferences";
     private static final String KEY_SET_COUNT_POSTFIX = "_count";
@@ -37,6 +41,13 @@ public final class SecurePreferences {
     private SecurePreferences() {
     }
 
+    /**
+     * Takes plain string value, encrypts it and stores it encrypted in the SecureStorage on the Android Device
+     *
+     * @param context Context is used internally
+     * @param key     Key used to identify the stored value in SecureStorage
+     * @param value   Plain String value that will be encrypted and stored in the SecureStorage
+     */
     public static void setValue(@NonNull Context context,
                                 @NonNull String key,
                                 @NonNull String value) throws SecureStorageException {
@@ -53,30 +64,65 @@ public final class SecurePreferences {
         }
     }
 
+    /**
+     * Takes plain string value, encrypts it and stores it encrypted in the SecureStorage on the Android Device
+     *
+     * @param context Context is used internally
+     * @param key     Key used to identify the stored value in SecureStorage
+     * @param value   Plain boolean value that will be encrypted and stored in the SecureStorage
+     */
     public static void setValue(@NonNull Context context,
                                 @NonNull String key,
                                 boolean value) throws SecureStorageException {
         setValue(context, key, String.valueOf(value));
     }
 
+    /**
+     * Takes plain string value, encrypts it and stores it encrypted in the SecureStorage on the Android Device
+     *
+     * @param context Context is used internally
+     * @param key     Key used to identify the stored value in SecureStorage
+     * @param value   Plain float value that will be encrypted and stored in the SecureStorage
+     */
     public static void setValue(@NonNull Context context,
                                 @NonNull String key,
                                 float value) throws SecureStorageException {
         setValue(context, key, String.valueOf(value));
     }
 
+    /**
+     * Takes plain string value, encrypts it and stores it encrypted in the SecureStorage on the Android Device
+     *
+     * @param context Context is used internally
+     * @param key     Key used to identify the stored value in SecureStorage
+     * @param value   Plain long value that will be encrypted and stored in the SecureStorage
+     */
     public static void setValue(@NonNull Context context,
                                 @NonNull String key,
                                 long value) throws SecureStorageException {
         setValue(context, key, String.valueOf(value));
     }
 
+    /**
+     * Takes plain string value, encrypts it and stores it encrypted in the SecureStorage on the Android Device
+     *
+     * @param context Context is used internally
+     * @param key     Key used to identify the stored value in SecureStorage
+     * @param value   Plain int value that will be encrypted and stored in the SecureStorage
+     */
     public static void setValue(@NonNull Context context,
                                 @NonNull String key,
                                 int value) throws SecureStorageException {
         setValue(context, key, String.valueOf(value));
     }
 
+    /**
+     * Takes plain string value, encrypts it and stores it encrypted in the SecureStorage on the Android Device
+     *
+     * @param context Context is used internally
+     * @param key     Key used to identify the stored value in SecureStorage
+     * @param value   Plain Set(type: String) value that will be encrypted and stored in the SecureStorage
+     */
     public static void setValue(@NonNull Context context,
                                 @NonNull String key,
                                 @NonNull Set<String> value) throws SecureStorageException {
@@ -88,6 +134,14 @@ public final class SecurePreferences {
         }
     }
 
+    /**
+     * Gets encrypted String value for given key from the SecureStorage on the Android Device, decrypts it and returns it
+     *
+     * @param context  Context is used internally
+     * @param key      Key used to identify the stored value in SecureStorage
+     * @param defValue Default String value that will be returned if the value with given key doesn't exist or an exception is thrown
+     * @return Decrypted String value associated with given key from SecureStorage
+     */
     @Nullable
     public static String getStringValue(@NonNull Context context,
                                         @NonNull String key,
@@ -105,30 +159,70 @@ public final class SecurePreferences {
         }
     }
 
+    /**
+     * Gets encrypted boolean value for given key from the SecureStorage on the Android Device, decrypts it and returns it
+     *
+     * @param context  Context is used internally
+     * @param key      Key used to identify the stored value in SecureStorage
+     * @param defValue Default boolean value that will be returned if the value with given key doesn't exist or an exception is thrown
+     * @return Decrypted boolean value associated with given key from SecureStorage
+     */
     public static boolean getBooleanValue(@NonNull Context context,
                                           @NonNull String key,
                                           boolean defValue) {
         return Boolean.parseBoolean(getStringValue(context, key, String.valueOf(defValue)));
     }
 
+    /**
+     * Gets encrypted float value for given key from the SecureStorage on the Android Device, decrypts it and returns it
+     *
+     * @param context  Context is used internally
+     * @param key      Key used to identify the stored value in SecureStorage
+     * @param defValue Default float value that will be returned if the value with given key doesn't exist or an exception is thrown
+     * @return Decrypted float value associated with given key from SecureStorage
+     */
     public static float getFloatValue(@NonNull Context context,
                                       @NonNull String key,
                                       float defValue) {
         return Float.parseFloat(getStringValue(context, key, String.valueOf(defValue)));
     }
 
+    /**
+     * Gets encrypted long value for given key from the SecureStorage on the Android Device, decrypts it and returns it
+     *
+     * @param context  Context is used internally
+     * @param key      Key used to identify the stored value in SecureStorage
+     * @param defValue Default long value that will be returned if the value with given key doesn't exist or an exception is thrown
+     * @return Decrypted long value associated with given key from SecureStorage
+     */
     public static long getLongValue(@NonNull Context context,
                                     @NonNull String key,
                                     long defValue) {
         return Long.parseLong(getStringValue(context, key, String.valueOf(defValue)));
     }
 
+    /**
+     * Gets encrypted int value for given key  from the SecureStorage on the Android Device, decrypts it and returns it
+     *
+     * @param context  Context is used internally
+     * @param key      Key used to identify the stored value in SecureStorage
+     * @param defValue Default int value that will be returned if the value with given key doesn't exist or an exception is thrown
+     * @return Decrypted int value associated with given key from SecureStorage
+     */
     public static int getIntValue(@NonNull Context context,
                                   @NonNull String key,
                                   int defValue) {
         return Integer.parseInt(getStringValue(context, key, String.valueOf(defValue)));
     }
 
+    /**
+     * Gets encrypted int value for given key  from the SecureStorage on the Android Device, decrypts it and returns it
+     *
+     * @param context  Context is used internally
+     * @param key      Key used to identify the stored value in SecureStorage
+     * @param defValue Default Set(type: String) value that will be returned if the value with given key doesn't exist or an exception is thrown
+     * @return Decrypted Set(type: String) value associated with given key from SecureStorage
+     */
     @NonNull
     public static Set<String> getStringSetValue(@NonNull Context context,
                                                 @NonNull String key,
@@ -147,6 +241,13 @@ public final class SecurePreferences {
         return res;
     }
 
+    /**
+     * Checks if SecureStorage contains a value for the given key (Does not return the value or check what type it is)
+     *
+     * @param context Context is used internally
+     * @param key     Key used to identify the stored value in SecureStorage
+     * @return True if value exists in SecureStorage, otherwise false
+     */
     public static boolean contains(@NonNull Context context,
                                    @NonNull String key) {
         Context applicationContext = context.getApplicationContext();
@@ -159,12 +260,24 @@ public final class SecurePreferences {
         }
     }
 
+    /**
+     * Removes the value for a given key from SecureStorage
+     *
+     * @param context Context is used internally
+     * @param key     Key used to identify the stored value in SecureStorage
+     */
     public static void removeValue(@NonNull Context context,
                                    @NonNull String key) {
         Context applicationContext = context.getApplicationContext();
         removeSecureValue(applicationContext, key);
     }
 
+    /**
+     * Clears all values from the SecureStorage on the Android Device and deletes the en/decryption keys
+     * Means new keys/keypairs have to be generated for the library to be able to work
+     *
+     * @param context Context is used internally
+     */
     public static void clearAllValues(@NonNull Context context) throws SecureStorageException {
         Context applicationContext = context.getApplicationContext();
         if (KeystoreTool.keyPairExists()) {
@@ -173,6 +286,12 @@ public final class SecurePreferences {
         clearAllSecureValues(applicationContext);
     }
 
+    /**
+     * Registers SecureStorageChangeListener to listen to any changes in SecureStorage
+     *
+     * @param context  Context is used internally
+     * @param listener Provided listener with given behaviour from the developer that will be registered
+     */
     public static void registerOnSharedPreferenceChangeListener(@NonNull Context context,
                                                                 @NonNull SharedPreferences.OnSharedPreferenceChangeListener listener) {
         Context applicationContext = context.getApplicationContext();
@@ -181,6 +300,12 @@ public final class SecurePreferences {
         preferences.registerOnSharedPreferenceChangeListener(listener);
     }
 
+    /**
+     * Unregisters SecureStorageChangeListener from SecureStorage
+     *
+     * @param context  Context is used internally
+     * @param listener Provided listener with given behaviour from the developer that will be unregistered
+     */
     public static void unregisterOnSharedPreferenceChangeListener(@NonNull Context context,
                                                                   @NonNull SharedPreferences.OnSharedPreferenceChangeListener listener) {
         Context applicationContext = context.getApplicationContext();
--- securestoragelibrary/src/main/java/de/adorsys/android/securestoragelibrary/SecureStorageException.java
+++ securestoragelibrary/src/main/java/de/adorsys/android/securestoragelibrary/SecureStorageException.java
@@ -16,27 +16,38 @@
 
 package de.adorsys.android.securestoragelibrary;
 
+/**
+ * Useful for catching possible exceptions thrown internally in the library so the user knows what to expect
+ */
+
 public class SecureStorageException extends Exception {
     public final ExceptionType type;
+
+    /**
+     * @param detailMessage Exception message that will be shown to developer when it is thrown
+     * @param cause         Cause of the throwable that has been caught (can be null)
+     * @param type          Type of exception that should be thrown
+     * @see ExceptionType
+     */
     public SecureStorageException(String detailMessage, Throwable cause, ExceptionType type) {
         super(detailMessage, cause);
         this.type = type;
     }
 
+    /**
+     * Used for throwing exceptions with a type that makes more sense to the developer based on the cause
+     */
     public enum ExceptionType {
         /**
-         * If this exception type is defined you cannot use the keystore / this library on the current device.
-         * This is fatal and most likely due to native key store issues.
+         * If this exception type is defined you cannot use the keystore / this library on the current device. This is fatal and most likely due to native key store issues.
          */
         KEYSTORE_EXCEPTION,
         /**
-         * If this exception type is defined a problem during encryption has occurred.
-         * Most likely this is due to using an invalid key for encryption or decryption.
+         * If this exception type is defined a problem during encryption has occurred. Most likely this is due to using an invalid key for encryption or decryption.
          */
         CRYPTO_EXCEPTION,
         /**
-         * If this exception type is set it means simply that the keystore cannot be used on the current device as it is not supported by this library.
-         * This probably means that you are targeting a device prior to api 23 or any not supported fingerprint sensors of Samsung
+         * If this exception type is set it means simply that the keystore cannot be used on the current device as it is not supported by this library. This probably means that you are targeting a device prior to api 23 or any not supported fingerprint sensors of Samsung
          */
         KEYSTORE_NOT_SUPPORTED_EXCEPTION,
         /**
