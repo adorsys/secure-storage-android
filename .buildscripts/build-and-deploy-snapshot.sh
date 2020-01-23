a682b1f4dbac25161b628ecb82ed831e4982793d
Drilon Reçica <drilonrecica.dev@gmail.com>
2020-01-23 14:17:08 +0100

    Update deployment script

--- .buildscripts/build-and-deploy-snapshot.sh
+++ .buildscripts/build-and-deploy-snapshot.sh
@@ -4,7 +4,7 @@ echo -e "\033[0;32m ./gradlew :securestoragelibrary:clean \033[0m"
 ./gradlew :securestoragelibrary:clean --info
 
 echo -e "\033[0;32m ./gradlew :securestoragelibrary:install \033[0m"
-./gradlew :securestoragelibrary:install --info
+./gradlew :securestoragelibrary:build --info
 
 echo -e "\033[0;32m ./gradlew :securestoragelibrary:bintrayUpload \033[0m"
 ./gradlew :securestoragelibrary:bintrayUpload -PbintrayUser= BINTRAY_USERNAME -PbintrayKey=BINTRAY_API_KEY -PdryRun=false --info
