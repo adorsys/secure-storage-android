8a0827dac38170542e84401d3804f329cdc6e744
Drilon Reçica <drilonrecica.dev@gmail.com>
2020-01-29 13:42:03 +0100

    Create MainActivity for showcasing app

---
+++ app/src/main/java/de/adorsys/android/securestoragetest/MainActivity.kt
@@ -0,0 +1,88 @@
+package de.adorsys.android.securestoragetest
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import de.adorsys.android.securestoragelibrary.SecureStorage
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_store.setOnClickListener {
+            when {
+                edit_text_store_key.text.isNullOrEmpty() || edit_text_store_value.text.isNullOrEmpty() ->
+                    Toast.makeText(
+                            this@MainActivity,
+                            "Fields cannot be empty",
+                            Toast.LENGTH_SHORT
+                    ).show()
+                else -> {
+                    SecureStorage.putString(
+                            this@MainActivity,
+                            edit_text_store_key.text.toString(),
+                            edit_text_store_value.text.toString()
+                    )
+
+                    text_view_stored_data.text =
+                            "Value: ${edit_text_store_value.text} successfully saved for key: ${edit_text_store_key.text}"
+
+                    edit_text_store_key.text.clear()
+                    edit_text_store_value.text.clear()
+
+                    text_view_deleted_data.text = ""
+                    text_view_retrieved_data.text = ""
+                }
+            }
+        }
+
+        button_get.setOnClickListener {
+            when {
+                edit_text_get_key.text.isNullOrEmpty() -> Toast.makeText(
+                        this@MainActivity,
+                        "Field cannot be empty",
+                        Toast.LENGTH_SHORT
+                ).show()
+                else -> {
+                    val decryptedData =
+                            SecureStorage.getString(
+                                    this@MainActivity,
+                                    edit_text_get_key.text.toString(),
+                                    "FAILED"
+                            )
+                    text_view_retrieved_data.text =
+                            "Decrypted Data for key: ${edit_text_get_key.text} = $decryptedData"
+
+                    edit_text_get_key.text.clear()
+                    text_view_deleted_data.text = ""
+                    text_view_stored_data.text = ""
+                }
+            }
+        }
+
+        button_remove.setOnClickListener {
+            when {
+                edit_text_remove_key.text.isNullOrEmpty() -> Toast.makeText(
+                        this@MainActivity,
+                        "Field cannot be empty",
+                        Toast.LENGTH_SHORT
+                ).show()
+                else -> {
+                    SecureStorage.remove(this@MainActivity, edit_text_remove_key.text.toString())
+
+                    text_view_deleted_data.text = "Value for key: ${edit_text_remove_key.text} successfully deleted"
+
+                    edit_text_remove_key.text.clear()
+                    text_view_retrieved_data.text = ""
+                    text_view_stored_data.text = ""
+                }
+            }
+        }
+
+    }
+}
